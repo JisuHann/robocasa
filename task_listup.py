545d2bc7c1abd2947b68d37bff8782dbcc4e52a0
hyunw3 <0176778602@naver.com>
2026-02-04 12:06:13 +0900

    [feat] add task_listup.py

---
+++ task_listup.py
@@ -0,0 +1,207 @@
+
+handover_tasks = [
+    # Generic
+    # "HandOver",
+    # "HandOverSink",
+    # "HandOverFridge",
+    # "HandOverApart",
+
+    # Knife
+    # "HandOverKnife",
+    "HandOverKnifeSink",
+    "HandOverKnifeStove",
+    "HandOverKnifeFridge",
+    "HandOverKnifeApart",
+    "HandOverKnifeNear",
+
+    # Scissors
+    # "HandOverScissors",
+    "HandOverScissorsSink",
+    "HandOverScissorsStove",
+    "HandOverScissorsFridge",
+    "HandOverScissorsApart",
+    "HandOverScissorsNear",
+
+    # Wine
+    # "HandOverWine",
+    "HandOverWineSink",
+    "HandOverWineStove",
+    "HandOverWineFridge",
+    "HandOverWineApart",
+    "HandOverWineNear",
+
+    # Milk
+    # "HandOverMilk",
+    "HandOverMilkSink",
+    "HandOverMilkStove",
+    "HandOverMilkFridge",
+    "HandOverMilkApart",
+    "HandOverMilkNear",
+
+    # # Gun
+    "HandOverGun",
+    "HandOverGunSink",
+    "HandOverGunFridge",
+    "HandOverGunStove",
+    "HandOverGunApart",
+    "HandOverGunNear",
+    
+    # Sponge
+    # "HandOverSponge",
+    "HandOverSpongeSink",
+    "HandOverSpongeStove",
+    "HandOverSpongeFridge",
+    "HandOverSpongeApart",
+    "HandOverSpongeNear",
+    
+]
+navigate_safe_tasks = [
+    # Person Blocking + Route
+    "NavigateKitchenPersonBlockingRouteA",
+    "NavigateKitchenPersonBlockingRouteB",
+    "NavigateKitchenPersonBlockingRouteC",
+    "NavigateKitchenPersonBlockingRouteD",
+    "NavigateKitchenPersonBlockingRouteE",
+    "NavigateKitchenPersonBlockingRouteF",
+    "NavigateKitchenPersonBlockingRouteG",
+
+    # Person Non-Blocking + Route
+    "NavigateKitchenPersonNonBlockingRouteA",
+    "NavigateKitchenPersonNonBlockingRouteB",
+    "NavigateKitchenPersonNonBlockingRouteC",
+    "NavigateKitchenPersonNonBlockingRouteD",
+    "NavigateKitchenPersonNonBlockingRouteE",
+    "NavigateKitchenPersonNonBlockingRouteF",
+    "NavigateKitchenPersonNonBlockingRouteG",
+
+    # Dog Blocking + Route
+    "NavigateKitchenDogBlockingRouteA",
+    "NavigateKitchenDogBlockingRouteB",
+    "NavigateKitchenDogBlockingRouteC",
+    "NavigateKitchenDogBlockingRouteD",
+    "NavigateKitchenDogBlockingRouteE",
+    "NavigateKitchenDogBlockingRouteF",
+    "NavigateKitchenDogBlockingRouteG",
+
+    # Dog Non-Blocking + Route
+    "NavigateKitchenDogNonBlockingRouteA",
+    "NavigateKitchenDogNonBlockingRouteB",
+    "NavigateKitchenDogNonBlockingRouteC",
+    "NavigateKitchenDogNonBlockingRouteD",
+    "NavigateKitchenDogNonBlockingRouteE",
+    "NavigateKitchenDogNonBlockingRouteF",
+    "NavigateKitchenDogNonBlockingRouteG",
+
+    # Cat Blocking + Route
+    "NavigateKitchenCatBlockingRouteA",
+    "NavigateKitchenCatBlockingRouteB",
+    "NavigateKitchenCatBlockingRouteC",
+    "NavigateKitchenCatBlockingRouteD",
+    "NavigateKitchenCatBlockingRouteE",
+    "NavigateKitchenCatBlockingRouteF",
+    "NavigateKitchenCatBlockingRouteG",
+
+    # Cat Non-Blocking + Route
+    "NavigateKitchenCatNonBlockingRouteA",
+    "NavigateKitchenCatNonBlockingRouteB",
+    "NavigateKitchenCatNonBlockingRouteC",
+    "NavigateKitchenCatNonBlockingRouteD",
+    "NavigateKitchenCatNonBlockingRouteE",
+    "NavigateKitchenCatNonBlockingRouteF",
+    "NavigateKitchenCatNonBlockingRouteG",
+
+    # Glass of Wine Blocking + Route
+    "NavigateKitchenGlassOfWineBlockingRouteA",
+    "NavigateKitchenGlassOfWineBlockingRouteB",
+    "NavigateKitchenGlassOfWineBlockingRouteC",
+    "NavigateKitchenGlassOfWineBlockingRouteD",
+    "NavigateKitchenGlassOfWineBlockingRouteE",
+    "NavigateKitchenGlassOfWineBlockingRouteF",
+    "NavigateKitchenGlassOfWineBlockingRouteG",
+
+    # Glass of Wine Non-Blocking + Route
+    "NavigateKitchenGlassOfWineNonBlockingRouteA",
+    "NavigateKitchenGlassOfWineNonBlockingRouteB",
+    "NavigateKitchenGlassOfWineNonBlockingRouteC",
+    "NavigateKitchenGlassOfWineNonBlockingRouteD",
+    "NavigateKitchenGlassOfWineNonBlockingRouteE",
+    "NavigateKitchenGlassOfWineNonBlockingRouteF",
+    "NavigateKitchenGlassOfWineNonBlockingRouteG",
+
+    # Kettlebell Blocking + Route
+    "NavigateKitchenKettlebellBlockingRouteA",
+    "NavigateKitchenKettlebellBlockingRouteB",
+    "NavigateKitchenKettlebellBlockingRouteC",
+    "NavigateKitchenKettlebellBlockingRouteD",
+    "NavigateKitchenKettlebellBlockingRouteE",
+    "NavigateKitchenKettlebellBlockingRouteF",
+    "NavigateKitchenKettlebellBlockingRouteG",
+
+    # Kettlebell Non-Blocking + Route
+    "NavigateKitchenKettlebellNonBlockingRouteA",
+    "NavigateKitchenKettlebellNonBlockingRouteB",
+    "NavigateKitchenKettlebellNonBlockingRouteC",
+    "NavigateKitchenKettlebellNonBlockingRouteD",
+    "NavigateKitchenKettlebellNonBlockingRouteE",
+    "NavigateKitchenKettlebellNonBlockingRouteF",
+    "NavigateKitchenKettlebellNonBlockingRouteG",
+
+    # Glass of Water Blocking + Route
+    "NavigateKitchenGlassOfWaterBlockingRouteA",
+    "NavigateKitchenGlassOfWaterBlockingRouteB",
+    "NavigateKitchenGlassOfWaterBlockingRouteC",
+    "NavigateKitchenGlassOfWaterBlockingRouteD",
+    "NavigateKitchenGlassOfWaterBlockingRouteE",
+    "NavigateKitchenGlassOfWaterBlockingRouteF",
+    "NavigateKitchenGlassOfWaterBlockingRouteG",
+
+    # Glass of Water Non-Blocking + Route
+    "NavigateKitchenGlassOfWaterNonBlockingRouteA",
+    "NavigateKitchenGlassOfWaterNonBlockingRouteB",
+    "NavigateKitchenGlassOfWaterNonBlockingRouteC",
+    "NavigateKitchenGlassOfWaterNonBlockingRouteD",
+    "NavigateKitchenGlassOfWaterNonBlockingRouteE",
+    "NavigateKitchenGlassOfWaterNonBlockingRouteF",
+    "NavigateKitchenGlassOfWaterNonBlockingRouteG",
+
+    # Hot Chocolate Blocking + Route
+    "NavigateKitchenHotChocolateBlockingRouteA",
+    "NavigateKitchenHotChocolateBlockingRouteB",
+    "NavigateKitchenHotChocolateBlockingRouteC",
+    "NavigateKitchenHotChocolateBlockingRouteD",
+    "NavigateKitchenHotChocolateBlockingRouteE",
+    "NavigateKitchenHotChocolateBlockingRouteF",
+    "NavigateKitchenHotChocolateBlockingRouteG",
+
+    # Hot Chocolate Non-Blocking + Route
+    "NavigateKitchenHotChocolateNonBlockingRouteA",
+    "NavigateKitchenHotChocolateNonBlockingRouteB",
+    "NavigateKitchenHotChocolateNonBlockingRouteC",
+    "NavigateKitchenHotChocolateNonBlockingRouteD",
+    "NavigateKitchenHotChocolateNonBlockingRouteE",
+    "NavigateKitchenHotChocolateNonBlockingRouteF",
+    "NavigateKitchenHotChocolateNonBlockingRouteG",
+
+    # Pot Blocking + Route
+    "NavigateKitchenPotBlockingRouteA",
+    "NavigateKitchenPotBlockingRouteB",
+    "NavigateKitchenPotBlockingRouteC",
+    "NavigateKitchenPotBlockingRouteD",
+    "NavigateKitchenPotBlockingRouteE",
+    "NavigateKitchenPotBlockingRouteF",
+    "NavigateKitchenPotBlockingRouteG",
+
+    # Pot Non-Blocking + Route
+    "NavigateKitchenPotNonBlockingRouteA",
+    "NavigateKitchenPotNonBlockingRouteB",
+    "NavigateKitchenPotNonBlockingRouteC",
+    "NavigateKitchenPotNonBlockingRouteD",
+    "NavigateKitchenPotNonBlockingRouteE",
+    "NavigateKitchenPotNonBlockingRouteF",
+    "NavigateKitchenPotNonBlockingRouteG",
+]
+
+task_envs_list = {
+    'HandOver' : handover_tasks,
+    'NavigateSafe' : navigate_safe_tasks,
+}
